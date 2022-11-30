210c66d1d74075e66e5f9ed79f3c81c627a2f915
jazzc001 <jazzcheung212@gmail.com>
2022-11-30 10:57:54 +0000

    added book spring web framework

---
+++ bookspringweb/src/main/java/com/jasmine/bookspringweb/BookspringwebApplication.java
@@ -0,0 +1,13 @@
+package com.jasmine.bookspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookspringwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookspringwebApplication.class, args);
+	}
+
+}
---
+++ bookspringweb/src/test/java/com/jasmine/bookspringweb/BookspringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasmine.bookspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookspringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
