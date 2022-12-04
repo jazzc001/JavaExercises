d59fd9b107ea918464d7a32571e1ed0c08c37f14
jazzc001 <jazzcheung212@gmail.com>
2022-12-04 22:42:53 +0000

    completed search book and list all book function in book project

--- bookspringweb/src/main/java/com/jasmine/bookspringweb/controller/BookController.java
+++ bookspringweb/src/main/java/com/jasmine/bookspringweb/controller/BookController.java
@@ -1,11 +1,15 @@
 package com.jasmine.bookspringweb.controller;
 
 
+import com.jasmine.bookspringweb.entity.Book;
 import com.jasmine.bookspringweb.model.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collection;
 
 @Controller
 public class BookController {
@@ -13,8 +17,43 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
-    @RequestMapping("/")
+    //===========Menu Page========================
+    @RequestMapping("/index")
     public ModelAndView menuPageController() {
         return new ModelAndView("index");
+    }
+
+    //==========List All Books=======
+    @RequestMapping("/listAll")
+    public ModelAndView listAllBooksPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        Collection<Book> bookList = bookService.getAllBooks();
+
+        modelAndView.addObject("books", bookList);
+        modelAndView.setViewName("ListAllBooks");
+
+        return modelAndView;
+    }
+
+    //========Search Book by ID======
+
+    @RequestMapping("/searchBookByIdPage")
+    public ModelAndView searchBookPage() {
+        return new ModelAndView("InputForSearchBookById");
+    }
+
+    @RequestMapping("/searchBookById")
+    public ModelAndView searchBookByIdController(@RequestParam("bookId")int id) {
+        ModelAndView modelAndView = new ModelAndView();
+        Book book = bookService.searchBookById(id);
+        if(book !=null) {
+            modelAndView.addObject("book", book);
+            modelAndView.setViewName("ShowBook");
+        }else {
+            modelAndView.addObject("message", "Book with ID: "+id+" does not exist.");
+            modelAndView.setViewName("Output");
+        }
+
+        return modelAndView;
     }
 }
